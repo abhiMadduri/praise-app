72810a21867bf30712c59335e1a0db5cbef2f29b
Abhishek Madduri <H174862@corp.halliburton.com>
2016-04-27 22:50:23 -0500

    initial commit of filter and pagination functionality

--- app/app.js
+++ app/app.js
@@ -2,7 +2,30 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', function($scope, $http) {
     
+    $scope.dataList = [];
+    $scope.nameFilter = null;
+    
+    $scope.praiseFilter = function(data) {
+        var keyword = new RegExp($scope.nameFilter, 'i');
+        return !$scope.nameFilter || keyword.test(data.Praises);
+    };
+    
     $http.get('data.json').success(function(data) {
-        $scope.dataArray = data;
+        $scope.dataList = data;
     });  
+    
+    //pagination
+    $scope.currentPage = 0;
+    $scope.pageSize = 10;
+    $scope.numberOfPages = function() {
+        return Math.ceil($scope.dataList.length/$scope.pageSize);
+    }
+       
+});
+
+myApp.filter('pagination', function() {
+   return function(input, start) {
+       start = +start;
+       return input.slice(start);
+   } 
 });
